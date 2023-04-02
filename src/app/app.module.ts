8ed5fc589a1fa81a0a3c2ee6c0bf7a9e74d346e6
GallardoN <nahuelgallardo2013@gmail.com>
2023-04-02 16:25:28 -0300

    Proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { HysComponent } from './components/hys/hys.component';
     ExperienciaComponent,
     EducacionComponent,
     HysComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
