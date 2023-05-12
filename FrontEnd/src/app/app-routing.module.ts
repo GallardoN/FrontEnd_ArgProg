18988ec03bd428cb3b6f005c97089636caf7fd21
GallardoN <nahuelgallardo2013@gmail.com>
2023-05-12 18:26:11 -0300

    Arreglos en proyectos

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
 import { NewSkillComponent } from './components/hys/new-skill.component';
 import { EditSkillComponent } from './components/hys/edit-skill.component';
 import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
+import { NewproyectComponent } from './components/proyectos/newproyect.component';
+import { EditproyectComponent } from './components/proyectos/editproyect.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -19,7 +21,9 @@ const routes: Routes = [
   {path:'editedu/:id', component: EditeducacionComponent},
   {path:'newskill', component: NewSkillComponent},
   {path:'editskill/:id', component: EditSkillComponent},
-  {path:'editacercade/:id', component: EditAcercaDeComponent}
+  {path:'editacercade/:id', component: EditAcercaDeComponent},
+  {path:'newproyect', component: NewproyectComponent},
+  {path:'editproyect/:id', component: EditproyectComponent}
 ];
 
 @NgModule({
